73d85942edb5617ead3780a32335b79e196120a3
Vitaliy K <vitaliyx@gmail.com>
2024-05-22 15:54:45 +0300

    Recursive TimeOut

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+
+const h1 = document.querySelector('h1');
+const button = document.querySelector('button');
+
+let id =setTimeout(tick, 3000);
+let counter = 0;
+
+function tick() {
+    h1.textContent = ++counter;
+   id = setTimeout(tick, 3000);
+
+}
+
+button.addEventListener('click', function () {
+    clearTimeout(id) })
+
+
