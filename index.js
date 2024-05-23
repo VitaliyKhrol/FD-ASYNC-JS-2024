66948e492b3b01d916a91e134142003c4cc7fd09
Vitaliy K <vitaliyx@gmail.com>
2024-05-23 22:17:25 +0300

    CreateCard with Promice

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 
 
 const resultofRequest = fetch('./data.json');
-console.log(resultofRequest);
 
-resultofRequest.then((responce)=>{
-  return responce.json()})
-  .then ((result)=>{
+resultofRequest
+  .then((responce) => {
+    return responce.json();
+  })
+  .then((result) => {
     console.log(result)
+    const card = createUserCard(result);
+    console.log(card);
+   const root =document.querySelector('#root');
+   root.append(card);
   })
-  .catch((data)=>{
-    console.log(data)
+  .catch((reject) => {
+    console.log(reject)
   })
 
+
+  function createUserCard(user){
+    const h2 = crElement('h2', {classNames: ['username']}, user.firstName,user.lastName);
+    const p = crElement('p',{}, user.telNum);
+    const div = crElement('div',{classNames:['card-wrapper']}, h2, p);
+    return div;
+    
+  }
+
+
+  function crElement(type,{classNames=[]}, ...children){
+    const elem = document.createElement(type);
+    elem.classList.add(...classNames);
+    elem.append(...children);
+    return elem;
+}
