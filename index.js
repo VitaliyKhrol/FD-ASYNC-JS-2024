ec479fe18a4f762182c329d184a339284d1d8ff0
Vitaliy K <vitaliyx@gmail.com>
2024-05-24 10:06:10 +0300

    Promise Creation

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
 
 
-const resultofRequest = fetch('./data.json');
+// const resultofRequest = fetch('./data.json');
 
-resultofRequest
-  .then((responce) => {
-    return responce.json();
-  })
-  .then((result) => {
-    console.log(result)
-    const card = createUserCard(result);
-    console.log(card);
-   const root =document.querySelector('#root');
-   root.append(card);
-  })
-  .catch((reject) => {
-    console.log(reject)
-  })
+// resultofRequest
+//   .then((responce) => {
+//     return responce.json();
+//   })
+//   .then((result) => {
+//     console.log(result)
+//     const card = createUserCard(result);
+//     console.log(card);
+//    const root =document.querySelector('#root');
+//    root.append(card);
+//   })
+//   .catch((reject) => {
+//     console.log(reject)
+//   })
 
 
-  function createUserCard(user){
-    const h2 = crElement('h2', {classNames: ['username']}, user.firstName,user.lastName);
-    const p = crElement('p',{}, user.telNum);
-    const div = crElement('div',{classNames:['card-wrapper']}, h2, p);
-    return div;
-    
+//   function createUserCard(user){
+//     const h2 = crElement('h2', {classNames: ['username']}, user.firstName,user.lastName);
+//     const p = crElement('p',{}, user.telNum);
+//     const div = crElement('div',{classNames:['card-wrapper']}, h2, p);
+//     return div;
+//   }
+
+
+//   function crElement(type,{classNames=[]}, ...children){
+//     const elem = document.createElement(type);
+//     elem.classList.add(...classNames);
+//     elem.append(...children);
+//     return elem;
+// }
+
+
+const promice = new Promise(function(resolve,reject){
+  if (Math.random() > 0.5){
+    resolve('Its Okey');
+  } else {
+    reject('its NOT okey');
   }
+});
+
+promice
+.then((res)=>{
+  console.log(res)
+})
+.catch((rev)=>{
+  console.log(rev)
+})
 
 
-  function crElement(type,{classNames=[]}, ...children){
-    const elem = document.createElement(type);
-    elem.classList.add(...classNames);
-    elem.append(...children);
-    return elem;
-}
+
