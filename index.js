c3c11ece129d9e6a692dfa4fbdeac5007e865321
Vitaliy K <vitaliyx@gmail.com>
2024-05-25 21:20:51 +0300

    Виджет прогноза погоді

--- index.js
+++ index.js
@@ -1,53 +1,62 @@
+// 096cec0f62991bf50404ef7a01cd627b
 
 
-const resultofRequest = fetch('https://dummyjson.com/products');
-const root =document.querySelector('#root');
-const spinner = document.createElement('div');
-spinner.classList.add('loader');
-root.append(spinner);
+const form = document.querySelector('form');
+
+const API_BASE = 'https://api.openweathermap.org/data/2.5/weather';
+// 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API_KEY}'
 
 
+const API_KEY = '096cec0f62991bf50404ef7a01cd627b';
 
-resultofRequest
-  .then((responce) => {
-  const arr = responce.json()
-    return arr;
-  })
-  .then((result) => {
-    console.log(result)
-    const productArray = result.products.map((user)=>createProductCard(user));
-    console.log(productArray);
-     root.append(...productArray);
-  })
-  .catch((reject) => {
-    console.log(reject)
-  })
-  .finally(()=>{
-    spinner.remove();
-  })
+form.addEventListener('submit', submitHandler);
 
+async function submitHandler(event){
+  event.preventDefault();
+  const city = event.target.city.value;
+  // console.log(city)
+  const url = `${API_BASE}?q=${city}&appid=${API_KEY}&units=metric&lang=ru`;
+  const weaterData = await (await fetch(url)).json();
+  // console.log(weaterData);
+  clearWidget();
+  const card = createWidget(weaterData,city);
+  const root = document.querySelector('#root')
+  root.append(card);
 
-  function createProductCard(product){
-
-    const h2 = crElement('h2', {classNames: ['username']}, product.title, product.category);
-    const img = document.createElement('img');
-    img.setAttribute('src', product.images[0] )
   
-    img.classList.add('avatar');
-    const p = crElement('p',{}, product.price);
-    const div = crElement('div',{classNames:['card-wrapper']}, h2,img, p);
-    return div;
-  }
-
-
-  function crElement(type,{classNames=[]}, ...children){
-    const elem = document.createElement(type);
-    elem.classList.add(...classNames);
-    elem.append(...children);
-    return elem;
 }
 
+function clearWidget(){
+  const section = document.querySelector('.weather');
+  if (section) {
+    section.remove();
+  }
+}
 
+function createWidget(weatherObj,city){
+  const cityN= crElement ('h1', {},'Город: ', getTranclate(city));
+  const description= crElement ('h2', {},'Облачность: ', weatherObj.weather[0].description );
+  const temp = crElement ('p',{},'Температура: ', weatherObj.main.temp );
+  const wind = crElement ('p', {}, 'Скорость ветра: ', weatherObj.wind.speed);
+  const widgetCard = crElement ('section', {classNames:['weather']},cityN,description,temp,wind);
+  return widgetCard;
 
+}
 
+function crElement(type,{classNames=[]}, ...children){
+  const elem = document.createElement(type);
+  elem.classList.add(...classNames);
+  elem.append(...children);
+  return elem;
+}
 
+function getTranclate(city){
+  const cityName = {
+    'Kharkiv': 'Харьков',
+    'Kyiv': 'Киев',
+    'Lviv': 'Львов',
+    'Zaporizhzhia' :'Запорожье',
+    'Dnipro' : 'Днепро',
+  }
+  return cityName[city];
+}
